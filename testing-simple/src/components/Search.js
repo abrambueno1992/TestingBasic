c3eb8e2ee60e9b69434a1f64ba7cafbd86c18630
Abraham Bueno <abueno@localhost.localdomain>
2018-12-05 21:06:08 -0600

    Initial setup with React and enzyme setup.

---
+++ testing-simple/src/components/Search.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Search extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+      return (
+        <div>
+          Inside the Search component.
+        </div>
+      )
+    }
+}
+
+export default Search;
