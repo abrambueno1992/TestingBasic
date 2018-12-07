e1f1c2f7d2893edc82c02382da34a20f90fdba71
Abraham Bueno <abueno@localhost.localdomain>
2018-12-07 00:27:14 -0600

    Adding more detail to the card component

--- testing-simple/src/components/CreateCard.js
+++ testing-simple/src/components/CreateCard.js
@@ -1,18 +1,29 @@
 import React, { Component } from "react";
-
+import image from "../caffeine.jpg";
 class CreateCard extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      suggestions: ["Funny", "Humor", "Coffee", "Problem", "Twisted humor"]
+    };
   }
   render() {
     return (
       <div>
-        <div>Card display image</div>
-        <div>List of cards already saved.</div>
-        <div>Suggested board name list</div>
+        <div>
+          <div>
+            <img src={image} />
+          </div>
+          <div>Image brief description of the image</div>
+        </div>
+        <div>List of cards images already saved.</div>
         <button>Create Board</button>
+        <div>
+          {this.state.suggestions.map(suggest => {
+            return <li>{suggest}</li>;
+          })}
+        </div>
         This is the create card component
       </div>
     );
